e10843576e1f95c8e35614ac5eab0318012cbcd6
ryooo <ryo13kb-13.7@docomo.ne.jp>
2017-05-14 10:37:04 +0000

    add column

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -52,11 +52,12 @@ class TasklistsController < ApplicationController
   
    def set_tasklist
     @tasklist = Tasklist.find(params[:id])
-    end
+   end
+
 
   # Strong Parameter
   def tasklist_params
-    params.require(:tasklist).permit(:content)
+    params.require(:tasklist).permit(:content,:status)
     
   end
 end
--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,3 +1,4 @@
 class Tasklist < ApplicationRecord
   validates :content, presence: true, length: { maximum: 10 }
+  validates :status, presence: true, length: { maximum: 10 }
 end
